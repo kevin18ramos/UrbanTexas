9becc7989ab0167d9bc3bf9ed6f84b9193a173fc
kevin18ramos <kevin.18ramos04@gmail.com>
2026-03-08 21:29:27 -0500

    flask works

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    template_folder="frontend",
+    static_folder="frontend"
+)
 
 @app.route("/")
 def home():
-    return Flask.render_template("index.html")
+    return render_template("home.html")
+
 
 @app.route("/cities")
 def cities():
@@ -13,6 +18,7 @@ def cities():
 @app.route("/login")
 def login():
     return "Login page coming soon"
+
 
 
 if __name__ == "__main__":
